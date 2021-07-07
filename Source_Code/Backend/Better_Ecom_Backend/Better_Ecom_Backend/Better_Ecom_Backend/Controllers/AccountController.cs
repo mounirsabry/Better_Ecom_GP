50051bffc00cfadd6f45440498057e5b020c754c
unknown <36314240+mounirsabry@users.noreply.github.com>
2021-07-07 23:19:01 +0200

    Added the project for DataLibrary
    Added the Library Class project called DataLibrary to the solution to separate the data access (mysql and dapper) from the backend part for better separate of concerns.
    
    Modifies the test controller to test a small query instead of a single string.
    
    Added the admin_user class to the modules.
    
    Changed AuthController class to AccountController for better matching the class diagram.
    
    Was working on the AccountController.

---
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Better_Ecom_Backend/Controllers/AccountController.cs
@@ -0,0 +1,119 @@
+﻿using Better_Ecom_Backend.Helpers;
+using Better_Ecom_Backend.Models;
+using DataLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Better_Ecom_Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IConfiguration _config;
+        private IDataAccess _data;
+
+        public AccountController(IConfiguration config, IDataAccess data)
+        {
+            _config = config;
+            _data = data;
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LogUser logUser)
+        {
+
+            //IActionResult
+            IActionResult response = Unauthorized();
+
+            bool exists = AuthenticateUser(logUser);
+
+            if (exists)
+            {
+                string tokenString = GenerateJSONWebToken(logUser);
+                response = Ok(new { token = tokenString});
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(LogUser logUser)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(ClaimTypes.Role, logUser.Type),
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              null,
+              claims,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private bool AuthenticateUser(LogUser logUser)
+        {
+            string table = "";
+            string sql = "";
+            var parameters = new
+            {
+                ID = logUser.ID,
+                password = logUser.Password,
+
+            };
+            DataAcess dataAcess = new DataAcess(_config.GetConnectionString(Constants.CurrentDBConnectionStringName));
+
+            string type = logUser.Type.ToLower();
+            string id_text = "";
+            switch (type)
+            {
+                case "student":
+                    table = "student";
+                    id_text = "student.student_id";
+                    break;
+                case "instructor":
+                    table = "instructor";
+                    id_text = "instructor.instructor_id";
+                    break;
+                case "admin":
+                    table = "admin_user";
+                    id_text = "admin_user.admin_user_id";
+                    break;
+                default:
+                    return false;
+            }
+            
+            sql = @$"SELECT system_user_id FROM {table} INNER JOIN system_user
+                    WHERE {id_text} = system_user.system_user_id 
+                    AND system_user.system_user_id = @ID AND system_user.user_password = @password";
+
+            List<int> rows = dataAcess.selectData<int, dynamic>(sql, parameters);
+
+            if (rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
---
+++ Source_Code/Backend/Better_Ecom_Backend/DataLibrary/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = connection.Query<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+
+        public void SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Execute(sql, parameters);
+            }
+        }
+    }
+}
