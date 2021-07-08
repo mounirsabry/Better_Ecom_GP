9ae31d8950656429e03ad59613a96ab710b99843
omar hassan <36304190+o3m7a7r@users.noreply.github.com>
2021-07-08 10:06:52 +0200

    .change password reuploaded
    due to some mistake i committed it again

--- Source_Code/Frontend/BetterEcomFrontend/src/app/modules/account-module/services/save-profile-changes.service.ts
+++ Source_Code/Frontend/BetterEcomFrontend/src/app/modules/account-module/services/save-profile-changes.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +13,25 @@ export class SaveProfileChangesService {
 
     this.httpClient.patch("",user);
   }
+
+  changePassword(oldPassword : string, newPassword : string)
+  {
+    return this.httpClient.patch<any>('',{"oldPassword" : oldPassword, "newPassword" : newPassword})
+  }
+}
+
+export function passwordValidator(control:AbstractControl): { [key :string] : boolean} {
+    const newPassword = control.get('newPassword');
+    const confirmPassword = control.get('confirmPassword');
+    if (newPassword.pristine || confirmPassword.pristine)
+    {
+      return null;
+    }
+    if(newPassword && confirmPassword && newPassword.value != confirmPassword.value)
+    {
+      console.log("hey")
+    }
+    return newPassword && confirmPassword && newPassword.value != confirmPassword.value ?
+      {'misMatch' : true} :
+      null;
 }
