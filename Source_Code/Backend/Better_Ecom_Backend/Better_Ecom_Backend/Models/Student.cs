126acaf7a497f448b79f8380a93fa9569102e491
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2021-07-08 17:03:46 +0200

    added constructor to handle json and GetUpdateQuery method

--- Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Models/Student.cs
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Models/Student.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Better_Ecom_Backend.Models
 {
-    public class Student
+    public class Student : System_user
     {
+        public Student(JsonElement data) : base(data)
+        {
+            
+            this.Student_id = data.GetProperty("student_id").GetInt32();
+            this.High_school_type = data.GetProperty("high_school_type").GetString();
+            this.Entrance_year = data.GetProperty("entrance_year").GetString();
+            this.GPA = data.GetProperty("gpa").GetDouble();
+            this.Department = data.GetProperty("department").GetString();
+            this.Academic_year = data.GetProperty("academic_year").GetInt32();
+
+
+        }
+
+        public override string GetUpdateQuery()
+        {
+            return @$"UPDATE student SET high_school_type = @High_school_type, entrance_year = @Entrance_year, gpa = @GPA, 
+                    department = @Department, academic_year = @Academic_year where student_id = @Student_id";
+        }
+
         public int Student_id { get; set; }
         public string High_school_type { get; set; }
         public string Entrance_year { get; set; }
-        public float Gpa { get; set; }
+        public double GPA { get; set; }
         public string Department { get; set; }
         public int Academic_year { get; set; }
 
