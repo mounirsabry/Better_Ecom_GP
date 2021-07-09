651fc1ed0c25459065cb5ca42ce469e7b9f6c185
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2021-07-09 19:40:51 +0200

    SecurityUtilities class has HashPassword() and Verify password methods.

---
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Helpers/SecurityUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BC = BCrypt.Net.BCrypt;
+
+namespace Better_Ecom_Backend.Helpers
+{
+    public class SecurityUtilities
+    {
+        public SecurityUtilities() { }
+        public static string HashPassword(string pass)
+        {
+
+            return BC.HashPassword(pass);
+        }
+
+        public static bool Verify(string normalPass, string hashedPass)
+        {
+            return BC.Verify(normalPass, hashedPass);
+        }
+    }
+
+  
+}
