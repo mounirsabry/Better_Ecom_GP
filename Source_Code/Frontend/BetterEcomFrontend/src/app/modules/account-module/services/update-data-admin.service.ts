67577dfd2e4d7c8b3a6d05f30643149a87e53c64
omar hassan <36304190+o3m7a7r@users.noreply.github.com>
2021-07-13 16:30:34 +0200

    .

--- Source_Code/Frontend/BetterEcomFrontend/src/app/modules/account-module/services/update-data-admin.service.ts
+++ Source_Code/Frontend/BetterEcomFrontend/src/app/modules/account-module/services/update-data-admin.service.ts
@@ -9,10 +9,10 @@ export class UpdateDataAdminService {
   constructor(private httpClient:HttpClient) { }
 
   createAccount(id : number){
-    return this.httpClient.patch<any>('', {"ID" : id})
+    return this.httpClient.patch<any>('https://localhost:44361/createAccount/' + localStorage.getItem('type'), {"ID" : id})
   }
 
   resetPassword(user : any){
-    return this.httpClient.patch<any>('', user)
+    return this.httpClient.patch<any>('https://localhost:44361/adminResetPassword', user)
   }
 }
