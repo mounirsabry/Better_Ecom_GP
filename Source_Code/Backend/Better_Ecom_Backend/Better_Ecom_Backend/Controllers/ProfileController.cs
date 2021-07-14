65636a05f5c14c709ab38b79c139340260b455a1
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2021-07-14 23:41:17 +0200

    returns password null

--- Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Controllers/ProfileController.cs
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Controllers/ProfileController.cs
@@ -50,9 +50,14 @@ namespace Better_Ecom_Backend.Controllers
                     + $"WHERE {id_text} = system_user.system_user_id" + "\n"
                     + "AND system_user.system_user_id = @ID;";
             var dbResult = _data.LoadData<dynamic, dynamic>(sql, new { ID = id }, _config.GetConnectionString("Default"));
-            
+
             if (dbResult != null)
-                return dbResult.FirstOrDefault();
+            {
+
+                var user = dbResult.FirstOrDefault();
+                user.user_password = null;
+                return user;
+            }
             else
                 return BadRequest(new { Message = "unknown error, maybe database server is down." });
         }
