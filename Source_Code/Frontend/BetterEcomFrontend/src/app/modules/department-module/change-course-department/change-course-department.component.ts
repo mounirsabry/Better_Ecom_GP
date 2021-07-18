0b4b041f9b10e639661089d8fc166ca67bd98c5f
omar hassan <36304190+o3m7a7r@users.noreply.github.com>
2021-07-18 18:01:50 +0200

    .

--- Source_Code/Frontend/BetterEcomFrontend/src/app/modules/department-module/change-course-department/change-course-department.component.ts
+++ Source_Code/Frontend/BetterEcomFrontend/src/app/modules/department-module/change-course-department/change-course-department.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DepartmentsService } from '../services/departments.service';
 
 @Component({
   selector: 'app-change-course-department',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeCourseDepartmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private departmentService : DepartmentsService) { }
+
+  view_department_list : Array<string> = []
+
+  changeCourseDepartmentForm = new FormGroup({
+    CourseID : new FormControl('', [Validators.required]),
+    NewDepartmentCode : new FormControl('', [Validators.required])
+  })
+
+  get courseIdGet(){
+    return this.changeCourseDepartmentForm.get('CourseID');
+  }
+
+  get departmentCodeGet(){
+    return this.changeCourseDepartmentForm.get('NewDepartmentCode');
+  }
 
   ngOnInit(): void {
+    this.departmentService.getDepartmentsData().subscribe(
+      data =>{
+        var department_names = data.map(function(obj) {return obj.department_name});
+        this.view_department_list = department_names;
+        //var department_code = data.map(function(obj) {return obj.department_code});
+        //this.department_code_list = department_code;
+      }
+    )
+  }
+
+  submitCourseToDepartment(){
+
   }
 
 }
