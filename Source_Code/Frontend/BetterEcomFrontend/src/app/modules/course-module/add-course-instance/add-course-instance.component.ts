f4fe522c97b215b00c3945bf7b4899fd15a6f98c
fares09 <fares9069@gmail.com>
2021-07-21 14:48:14 +0200

    add skeleton for course_instance and register in course instance

---
+++ Source_Code/Frontend/BetterEcomFrontend/src/app/modules/course-module/add-course-instance/add-course-instance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-course-instance',
+  templateUrl: './add-course-instance.component.html',
+  styleUrls: ['./add-course-instance.component.css']
+})
+export class AddCourseInstanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Source_Code/Frontend/BetterEcomFrontend/src/app/modules/course-module/register-student-instructor-in-a-course/register-student-instructor-in-a-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-student-instructor-in-a-course',
+  templateUrl: './register-student-instructor-in-a-course.component.html',
+  styleUrls: ['./register-student-instructor-in-a-course.component.css']
+})
+export class RegisterStudentInstructorInACourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
