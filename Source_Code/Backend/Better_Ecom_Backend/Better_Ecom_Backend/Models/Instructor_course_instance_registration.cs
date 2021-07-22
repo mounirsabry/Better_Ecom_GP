352de32aeb7cd0174fdd4ea279422c992635eba9
unknown <36314240+mounirsabry@users.noreply.github.com>
2021-07-23 01:01:45 +0200

    Finished Instructor Course Instance Registration Model

--- Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Models/Instructor_course_instance_registration.cs
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Models/Instructor_course_instance_registration.cs
@@ -29,7 +29,30 @@ namespace Better_Ecom_Backend.Models
                 Registration_id = temp.GetProperty(nameof(Registration_id)).GetInt32();
             else
                 Registration_id = -1;
+
+            if (jsonInput.TryGetProperty(nameof(Instructor_id), out temp) && temp.TryGetInt32(out _))
+                Instructor_id = temp.GetProperty(nameof(Instructor_id)).GetInt32();
+            else
+                Instructor_id = -1;
+
+            if (jsonInput.TryGetProperty(nameof(Course_instance_id), out temp) && temp.TryGetInt32(out _))
+                Course_instance_id = temp.GetProperty(nameof(Course_instance_id)).GetInt32();
+            else
+                Course_instance_id = -1;
+
+            if (jsonInput.TryGetProperty(nameof(Registration_date), out temp) && temp.TryGetDateTime(out _))
+                Registration_date = temp.GetProperty(nameof(Registration_date)).GetDateTime();
+            else
+                Registration_date = DateTime.MinValue;
         }
 
+        public void Print()
+        {
+            Console.WriteLine("Instructor Course Instance Registration Info.");
+            Console.WriteLine($"Registration ID :{ Registration_id }.");
+            Console.WriteLine($"Instructor ID :{ Instructor_id }.");
+            Console.WriteLine($"Course Instance ID :{ Course_instance_id }.");
+            Console.WriteLine($"Registration Date :{ Registration_date }.");
+        }
     }
 }
