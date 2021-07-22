816662b1a59e66033ecc9b7516741499c6976c1b
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2021-07-22 21:24:01 +0200

    Update CourseController.cs

--- Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Controllers/CourseController.cs
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Controllers/CourseController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 
 namespace Better_Ecom_Backend.Controllers
