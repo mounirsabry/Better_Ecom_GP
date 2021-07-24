e79a4ed5ea5e2027158802c74f3ab8828a074203
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2021-07-24 21:12:37 +0200

    .

---
+++ Source_Code/Backend/Better_Ecom_Backend/Better_Ecom_Backend/Models/GpaData.cs
@@ -0,0 +1,20 @@
+﻿using Better_Ecom_Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Better_Ecom_Backend.Models
+{
+    public class GpaData
+    {
+        public double Credit_hours { get; set; }
+        public StudentCourseInstanceGrade Student_course_instance_grade { get; set; }
+
+        public GpaData (int creditHours, StudentCourseInstanceGrade grade)
+        {
+            this.Credit_hours = creditHours;
+            this.Student_course_instance_grade = grade;
+        }
+    }
+}
